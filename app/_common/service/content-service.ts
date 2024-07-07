9ca85a63262191b5919e8e34b83de0d2ca691eca
ambashtalk <lohityakumarambashta@gmail.com>
2024-07-08 00:13:18 +0530

    feat: create content service

---
+++ app/_common/service/content-service.ts
@@ -0,0 +1,25 @@
+import CMSClient from "../clients/cms-client";
+
+export class ContentService {
+  private static instance: ContentService;
+  private cmsClient: CMSClient;
+
+   // Private constructor to prevent direct instantiation
+   private constructor() {
+     // Initialize your service here
+     this.cmsClient = CMSClient.getInstance()
+  }
+
+  // Static method to get the single instance of the class
+  public static getInstance(): ContentService {
+    if (!ContentService.instance) {
+      ContentService.instance = new ContentService();
+    }
+    return ContentService.instance;
+  }
+
+  public getHeaderSection() {
+    
+  }
+
+}
