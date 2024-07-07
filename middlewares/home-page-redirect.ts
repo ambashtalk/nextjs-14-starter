fbb92cf76c5a8f24c8b2c9892c2c54ce02da75b3
ambashtalk <lohityakumarambashta@gmail.com>
2024-07-07 10:54:17 +0530

    feat: create home route middleware

---
+++ middlewares/home-page-redirect.ts
@@ -0,0 +1,8 @@
+import { NextMiddleware, NextResponse } from "next/server";
+
+// Redirect to home page when user directly visits server root
+export const handleHomePageRedirect: NextMiddleware = (request) => {
+  if (request.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
+};
